bb860dde4db0423bd653bc4154c71cbd9138b863
s.alimardani <sobhan8181>
2025-03-05 18:43:45 +0330

    main : add TaxForecasterApplication class on @SpringBootTest

--- src/test/java/com/snappay/taxforecaster/TaxForecasterApplicationTests.java
+++ src/test/java/com/snappay/taxforecaster/TaxForecasterApplicationTests.java
@@ -3,7 +3,7 @@ package com.snappay.taxforecaster;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TaxForecasterApplication.class)
 class TaxForecasterApplicationTests {
 
 	@Test
