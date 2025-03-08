ab0e1e7a96402cae60c1ee9dc5e5491a000b49fe
Sobhan <lijavawork@gmail.com>
2025-03-08 10:02:04 +0330

    main : handle runtime exception and refactor jmeter

--- src/main/java/com/snappay/taxforecaster/common/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/snappay/taxforecaster/common/exception/ExceptionControllerAdvice.java
@@ -1,14 +1,22 @@
 package com.snappay.taxforecaster.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Collections;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler(NotAcceptableException.class)
     public ResponseEntity<ErrorModel> handle(NotAcceptableException exception) {
-        return new ResponseEntity<>(new ErrorModel(exception.getMessages()),exception.getStatus());
+        return new ResponseEntity<>(new ErrorModel(exception.getMessages()), exception.getStatus());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorModel> handle(RuntimeException exception) {
+        return new ResponseEntity<>(new ErrorModel(Collections.singletonList(exception.getMessage())), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
